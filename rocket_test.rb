44e433455d45910f95a3ced0efe6cae50c5b50df
Doug <doug.steinberg@gmail.com>
2017-06-23 15:32:09 -0400

    Exercise complete

--- rocket_test.rb
+++ rocket_test.rb
@@ -19,4 +19,49 @@ class RocketTest < Minitest::Test
     assert @rocket.colour == "blue"
   end
 
+  def test_flying_false
+    # @rocket = Rocket.new(flying: false)
+    assert @rocket.flying? == false
+  end
+
+  def test_flying_true
+    @rocket = Rocket.new(flying: true)
+    assert @rocket.flying? == true
+  end
+
+  def test_lift_off_while_flying
+    @rocket.lift_off
+    assert @rocket.lift_off == false
+  end
+
+  def test_lift_off_while_not_flying
+    assert @rocket.lift_off == true
+  end
+
+  def test_land_while_flying
+    @rocket.lift_off
+    assert @rocket.land == true
+  end
+
+  def test_land_while_flying_grounds_rocket
+    @rocket = Rocket.new(flying: true)
+    @rocket.land
+    assert @rocket.flying? == false
+  end
+
+  def test_land_while_not_flying
+    assert @rocket.land == false
+  end
+
+  def test_status_while_flying
+    name = @rocket.name
+    @rocket.lift_off
+    assert_equal @rocket.status, "Rocket #{name} is flying through the sky!"
+  end
+
+  def test_status_while_not_flying
+    name = @rocket.name
+    assert_equal @rocket.status, "Rocket #{name} is ready for lift off!"
+  end
+
 end
