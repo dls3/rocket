8edd0d4c099f607eafa4674c3264db8b6758bca2
Doug <doug.steinberg@gmail.com>
2017-06-23 00:01:48 -0400

    Tests for new name and color

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,20 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_new_name
+    # @rocket = Rocket.new
+    @rocket.name = "apollo"
+    assert @rocket.name == "apollo"
+  end
+
+  def test_new_colour
+    # @rocket = Rocket.new
+    @rocket.colour = "blue"
+    assert @rocket.colour == "blue"
+  end
+
 end
